a929aafc4eddc65d409cf977b27f6060232d8e42
Jessica Dao <xjessicadao@gmail.com>
2021-02-19 14:16:26 -0800

    restart

--- schema.sql
+++ schema.sql
@@ -6,22 +6,23 @@ USE employee_db;
 CREATE TABLE department (
     id INT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY AUTO AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 
-)
+);
+
 CREATE TABLE employee (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT
-    FOREIGN KEY(role_id) REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY(role_id) REFERENCES roles(id),
     FOREIGN KEY(manager_id) REFERENCES employee(id)
 )
